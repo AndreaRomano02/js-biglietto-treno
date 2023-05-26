6468a15ec9ee59d6640c0fa5ad9db5364245a8c1
AndreaRomano02 <andy.romano2002@gmail.com>
2023-05-26 15:18:58 +0200

    Add Principal Steps

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log('JS OK');
+
+//# PRELIMINARI
+const priceForKm = 0.21;
+const discountMinor = 0.8;
+const discountOver = 0.6;
+console.log(priceForKm, discountMinor, discountOver);
+
+let priceKmDiscounted;
+//# Chiedo all'utente le informazioni
+const userKm = parseInt(prompt('Quanti Km vuoi percorrere?', 10));
+const userAge = parseInt(prompt('Quanti anni hai?', 20));
+console.log(userKm, userAge);
+
+//# Calcolo della tariffa base
+let basePriceKm = userKm * priceForKm;
+
+//# Controllo l'età ed applico lo sconto
+if(userAge < 18){
+basePriceKm = basePriceKm * discountMinor;
+} else if(userAge >= 65){
+  basePriceKm = basePriceKm * discountOver;
+}
+
+//# Arrotondo a due decimali massimi
+basePriceKm = basePriceKm.toFixed(2) + '€';
+
+//# Stampo il prezzo finale 
+console.log(basePriceKm);
